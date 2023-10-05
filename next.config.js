593672d0f20af1a801ac7586f868fe7a49926db4
yudhawan <yudhaashter@gmail.com>
2023-10-05 14:33:50 +0700

    next config

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // output:'export',
-    images: {
-      unoptimized:true,
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: process.env.URL,
-            port: '',
-            pathname: '/products/img/**',
-          },
-        ],
-    },
-    env:{
-        BASE_URL: process.env.BASE_URL
-    }
+    // images: {
+    //   unoptimized:true,
+    //     remotePatterns: [
+    //       {
+    //         protocol: 'https',
+    //         hostname: process.env.URL,
+    //         port: '',
+    //         pathname: '/products/img/**',
+    //       },
+    //     ],
+    // },
+    // env:{
+    //     BASE_URL: process.env.BASE_URL
+    // }
 }
 
 module.exports = nextConfig
